5db7ad7e74127cfc4a435c28c92581df6d3bf105
tbeliyah <tomer.b.eliyahu@intel.com>
2021-05-02 19:22:43 +0300

    fix the build

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		TOMER BREAK THE BUILD
+		System.out.println("TOMER BREAK THE BUILD");
 	}
 
 	@Bean
